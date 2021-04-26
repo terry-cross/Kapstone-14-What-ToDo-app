1e6dde7c41aa7e0ba61a956be19e08a5791644ad
Jared Fajman <jmfajman@gmail.com>
2021-04-26 11:16:47 -0700

    Users filter

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,31 +1,31 @@
-import { Link } from "react-router-dom";
-import TodoItem from "./TodoItem";
-import button from "react-bootstrap";
+// import { Link } from "react-router-dom";
+// import TodoItem from "./TodoItem";
+// import button from "react-bootstrap";
 
-function TodoList(props) {
-  console.log(props);
-  return (
-    <section className="main">
-      <Link to={"/profile"}>
-        <p style={{ position: "relative", right: "650px", bottom: "150px" }}>
-          Profile
-        </p>
-      </Link>
+// function TodoList(props) {
+//   console.log(props);
+//   return (
+//     <section className="main">
+//       <Link to={"/profile"}>
+//         <p style={{ position: "relative", right: "650px", bottom: "150px" }}>
+//           Profile
+//         </p>
+//       </Link>
 
-      <ul className="todo-list">
-        {props.todos.map((todo) => (
-          <TodoItem
-            title={todo.title}
-            completed={todo.completed}
-            id={todo.id}
-            key={todo.id}
-            handleCheck={props.handleCheck}
-            handleDelete={props.handleDelete}
-          />
-        ))}
-      </ul>
-    </section>
-  );
-}
+//       <ul className="todo-list">
+//         {props.todos.map((todo) => (
+//           <TodoItem
+//             title={todo.title}
+//             completed={todo.completed}
+//             id={todo.id}
+//             key={todo.id}
+//             handleCheck={props.handleCheck}
+//             handleDelete={props.handleDelete}
+//           />
+//         ))}
+//       </ul>
+//     </section>
+//   );
+// }
 
-export default TodoList;
+// export default TodoList;
